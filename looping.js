0225c9a4f013ae299ad604a76e4cf9a0856cdc1b
herman88b <140402665+herman88b@users.noreply.github.com>
2023-09-30 12:45:46 +0800

    Add files via upload

---
+++ looping.js
@@ -0,0 +1,10 @@
+
+
+for ( let i= 1; i < 50; i++){
+    if ( i % 2 === 0 ) { //pengkondisian jika bilangan dibagi 2 = 0 makaakan menjadi genap dan sebaliknya
+        console.log ( i + "genap")
+    }
+    else {
+        console.log( i + "ganjil")
+    }
+}
